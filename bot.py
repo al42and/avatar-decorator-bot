a1afedd6ec09d97b0423c3b13ecf37c374f7afaa
Andrey Alekseenko <al42and@gmail.com>
2017-09-22 16:29:27 +0300

    Decorate received photos too, not only avatars

--- bot.py
+++ bot.py
@@ -5,8 +5,7 @@ import io
 
 import config
 import graphics
-from db import Color, initialize_database
-
+import db
 from telegram import ReplyKeyboardMarkup, KeyboardButton
 from telegram import ParseMode
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
@@ -16,7 +15,7 @@ REFRESH_KEYWORD = '⟳'
 
 
 def _generate_keyboard():
-    colors_iter = Color.select()
+    colors_iter = db.Color.select()
     buttons = [KeyboardButton(color.name) for color in colors_iter] + [KeyboardButton(REFRESH_KEYWORD)]
     keyboard = list(zip(buttons[0::2], buttons[1::2]))
     if len(buttons) % 2 == 1:
@@ -40,7 +39,11 @@ def _get_user_avatar(bot, user):
         return None
     else:
         biggest_avatar = avatars.photos[0][-1]
-        avatar_id = biggest_avatar.file_id
+        return _get_image_from_photo(bot, biggest_avatar)
+
+
+def _get_image_from_photo(bot, photo):
+        avatar_id = photo.file_id
         fd = io.BytesIO()
         bot.get_file(avatar_id).download(out=fd)
         fd.seek(0)
@@ -62,6 +65,15 @@ def _send_updated_avatar(bot, update, color):
     bot.send_photo(photo=image, chat_id=chat_id)
 
 
+def _update_last_user_choice(user_id, color):
+    try:
+        lc = db.LastUserChoice.get(db.LastUserChoice.user_id == user_id)
+        lc.color = color
+        lc.save()
+    except db.LastUserChoice.DoesNotExist:
+        db.LastUserChoice.create(user_id=user_id, color=color)
+
+
 def handler_start(bot, update):
     logging.info('%s started the bot', update.effective_user.name)
     update.message.reply_text('Привет! Этот бот добавляет цветные кружки к аватаркам.')
@@ -72,9 +84,10 @@ def handler_start(bot, update):
 def handler_help(bot, update):
     update.message.reply_text(
         'Выбери свой экипаж, и бот сгенерирует тебе новую аватарку с цветной окантовкой!\n'
+        'А ещё можно скидывать ему картинки, и он их раскрасит в цвета последнего выбранного экипажа.\n'
         'Добавить/поменять окрас: /set _экипаж_ _цвет_.\n'
         'Удалить окрас: /rm _экипаж_.\n'
-        'Цвет можно задавать английскими словами, `#rrggbb` или м.б. ещё как.',
+        'Цвет можно задавать английскими словами, `#rrggbb` или м.б. ещё как.\n',
         parse_mode=ParseMode.MARKDOWN)
 
 
@@ -84,9 +97,9 @@ def handler_set(bot, update, args):
         name = args[0]
         rgb = graphics.get_color(args[1:])
         try:
-            color = Color.get(name=name)
-        except Color.DoesNotExist:
-            color = Color.create(name=name, r=0, g=0, b=0)
+            color = db.Color.get(name=name)
+        except db.Color.DoesNotExist:
+            color = db.Color.create(name=name, r=0, g=0, b=0)
         color.r, color.g, color.b = rgb
         color.save()
     except (IndexError, ValueError):
@@ -100,11 +113,11 @@ def handler_rm(bot, update, args):
     logging.info('Removing color (by %s): %s', update.effective_user.name, ' '.join(args))
     try:
         name = args[0]
-        color = Color.get(name=name)
+        color = db.Color.get(name=name)
         color.delete_instance()
     except (IndexError, ValueError):
         update.message.reply_text('Использование: /rm _экипаж_', parse_mode=ParseMode.MARKDOWN)
-    except Color.DoesNotExist:
+    except db.Color.DoesNotExist:
         update.message.reply_text('Не могу найти такой экипаж')
     else:
         update.message.reply_text('Готово!')
@@ -118,11 +131,30 @@ def handler_message(bot, update):
         _refresh_keyboard(update)
     else:
         try:
-            color = Color.get(Color.name == data)
-        except Color.DoesNotExist:
+            color = db.Color.get(db.Color.name == data)
+        except db.Color.DoesNotExist:
             _refresh_keyboard(update)
         else:
             _send_updated_avatar(bot, update, color)
+            _update_last_user_choice(update.effective_user.id, color)
+
+
+def handler_photo(bot, update):
+    photo = update.message.photo[-1]  # Last element is the biggest
+    user = update.effective_user
+    logging.info('Got photo from %s', user.name)
+    try:
+        color = db.LastUserChoice.get(db.LastUserChoice.user_id == user.id).color
+    except db.LastUserChoice.DoesNotExist:
+        update.message.reply_text('Сначала выбери экипаж, а потом пошли фотку! (Да, я при этом пришлю обработанную аватарку. Но мне не лень!)')
+    except db.Color.DoesNotExist:
+        update.message.reply_text('Выбери новый экипаж и пошли фотку снова!')
+    else:
+        rgb = (color.r, color.g, color.b)
+        image = _get_image_from_photo(bot, photo)
+        image = graphics.add_color_to_avatar(image, rgb)
+        logging.info('Sending updated avatar to %s', user.name)
+        bot.send_photo(photo=image, chat_id=update.effective_chat.id)
 
 
 def error(bot, update, error):
@@ -131,7 +163,7 @@ def error(bot, update, error):
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-    initialize_database()
+    db.initialize_database()
 
     updater = Updater(config.TOKEN)
 
@@ -140,6 +172,7 @@ if __name__ == '__main__':
     updater.dispatcher.add_handler(CommandHandler('set', handler_set, pass_args=True))
     updater.dispatcher.add_handler(CommandHandler('rm', handler_rm, pass_args=True))
     updater.dispatcher.add_handler(MessageHandler(Filters.text, handler_message))
+    updater.dispatcher.add_handler(MessageHandler(Filters.photo, handler_photo))
     updater.dispatcher.add_error_handler(error)
 
     updater.start_polling(poll_interval=5)
--- db.py
+++ db.py
@@ -13,6 +13,15 @@ class Color(Model):
     b = IntegerField(constraints=[Check('b >= 0 and b < 256')])
 
 
+class LastUserChoice(Model):
+    class Meta:
+        database = database
+
+    user_id = IntegerField(unique=True)
+    color = ForeignKeyField(rel_model=Color, on_delete='CASCADE')
+
+
 def initialize_database():
     database.connect()
     Color.create_table(fail_silently=True)
+    LastUserChoice.create_table(fail_silently=True)
